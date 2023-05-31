5a0316ca8af8be96a6c67314744f5e11c10a56e2
guidomidolo <gui.midolo@hotmail.com>
2023-05-30 23:56:06 -0300

    link en logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import NavBar from "./NavBar";
 import LogoRazer from "./images/logoRazer.png"
 import CartWidget from "./CartWidget";
+import { Link } from "react-router-dom";g
 
 const Header = () => {
     return (
         <div className="container-fluid">
             <div className="row bg-dark align-items-center"  style={{borderBottom: "2px solid #44d62c"}}>
                 <div className="col-md-2 text-center">
-                    <img src={LogoRazer} alt={"Razer Gaming x Coderhouse"} width={150}/>
+                <Link to={"/"}><img src={LogoRazer} alt={"Razer Gaming x Coderhouse"} width={150}/></Link>
                 </div>
                 <div className="col-md-9 text-center">
                     <NavBar fuente={"RazerF5"}/>
