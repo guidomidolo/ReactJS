1e43051ce660a2efea8850abda709f370f09536c
guidomidolo <gui.midolo@hotmail.com>
2023-06-18 02:11:28 -0300

    instalacion y pruebas de firebase

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,30 +1,42 @@
 import { useState, useEffect } from "react";
 import ItemList from "./ItemList";
-import productos from "./json/productos.json";
+// import productos from "./json/productos.json";
 import { useParams } from "react-router-dom";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 
 const ItemListContainer = () => {
     const [datos, setDatos] = useState([]);
     const {id} = useParams();
+    
+    // Acceder a los productos desde JSON
+    // useEffect(() => {
+    //     const promesa = new Promise((resolve) => {
+    //         setTimeout(() => {
+    //             resolve(id ? productos.filter(item => item.categoria === id) : productos);
+    //         }, 2000);
+    //     });
 
-    useEffect(() => {
-        const promesa = new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(id ? productos.filter(item => item.categoria === id) : productos);
-            }, 2000);
-        });
+    //     promesa.then(data => {
+    //         setDatos(data);
+    //     });
+    // }, [id]);
 
-        promesa.then(data => {
-            setDatos(data);
-        });
-    }, [id]);
+    // Acceder a los productos desde Firestone
+        useEffect(() => {
+            const db = getFirestore();
+            const producto = doc(db, "items", "GU6NMV51WzYFfw8dpajj")
+            getDoc(producto).then(resultado => {
+                console.log(resultado);
+                console.log("ID del producto: " + resultado.id);
+            })
+    }, []);
 
     return (
         <div className="container">
             <div className="row py-5">
                 <h4 className="pt-3" style={{ fontFamily: "RazerF5" , fontSize : 40}}>Products</h4>
-                <ItemList listadoItems={datos}/>          
+                {/* <ItemList listadoItems={datos}/>           */}
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqQn5Ijc4jiWtjIKJhxXCZ0xcS1-2crCE",
+  authDomain: "razercoderhouse.firebaseapp.com",
+  projectId: "razercoderhouse",
+  storageBucket: "razercoderhouse.appspot.com",
+  messagingSenderId: "1062643158387",
+  appId: "1:1062643158387:web:32311f19d4032e91a00906"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
